ce885ec7595b089306941d06b27e21a8388ba296
Soulaimane02 <zelloufi.soulaimane@gmail.com>
2025-01-25 22:12:18 +0100

    terminaison des fonctions de changement de page

--- src/main/java/appli/schumanconnect/controller/HomeController.java
+++ src/main/java/appli/schumanconnect/controller/HomeController.java
@@ -5,8 +5,14 @@ import appli.schumanconnect.utils.ScenePage;
 import appli.schumanconnect.utils.UserConnectedSingleton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -31,15 +37,25 @@ public class HomeController implements Initializable {
     }
 
     @FXML
+    public void changePageSceneHome(ActionEvent event) throws IOException {
+        ScenePage.switchView("/appli/schumanconnect/homeView/homePage-view.fxml", event);
+    }
+
+    @FXML
     public void logout(ActionEvent event) throws IOException {
         UserConnectedSingleton.getInstance().logout();
         ScenePage.switchView("/appli/schumanconnect/login-view.fxml", event);
     }
 
     @FXML
-    public void changePageToAllStudent(ActionEvent event) throws IOException {
-        ScenePage.switchView("/appli/schumanconnect/secretaryView/allStudents.fxml", event);
+    public void changePageSceneDossierInscription(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/appli/schumanconnect/secretaryView/allStudents.fxml"));
+        Parent root = loader.load();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        Stage stage = (Stage) menuItem.getParentPopup().getOwnerWindow();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
     }
-
-
 }
--- src/main/java/appli/schumanconnect/controller/SecretaryController/AllStudentsController.java
+++ src/main/java/appli/schumanconnect/controller/SecretaryController/AllStudentsController.java
@@ -176,10 +176,29 @@ public class AllStudentsController implements Initializable {
     }
 
     @FXML
+    public void changePageSceneHome(ActionEvent event) throws IOException {
+        ScenePage.switchView("/appli/schumanconnect/homeView/homePage-view.fxml", event);
+    }
+
+    @FXML
     public void logout(ActionEvent event) throws IOException {
         UserConnectedSingleton.getInstance().logout();
         ScenePage.switchView("/appli/schumanconnect/login-view.fxml", event);
     }
+
+    @FXML
+    public void changePageSceneDossierInscription(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/appli/schumanconnect/secretaryView/allStudents.fxml"));
+        Parent root = loader.load();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        Stage stage = (Stage) menuItem.getParentPopup().getOwnerWindow();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
 
 
