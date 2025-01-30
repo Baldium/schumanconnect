7ed1fbfd89df84576a1e82b86c37ae7a683ad16d
Soulaimane02 <zelloufi.soulaimane@gmail.com>
2025-01-30 21:53:52 +0100

    ajout de la page statique pour remplir le dossier et le creer

--- src/main/java/appli/schumanconnect/controller/SecretaryController/AssemblyDossierController.java
+++ src/main/java/appli/schumanconnect/controller/SecretaryController/AssemblyDossierController.java
@@ -1,17 +1,21 @@
 package appli.schumanconnect.controller.SecretaryController;
 import appli.schumanconnect.model.Dossier;
 import appli.schumanconnect.model.Student;
-import appli.schumanconnect.utils.Bdd;
-import appli.schumanconnect.utils.DossierSingleton;
-import appli.schumanconnect.utils.ScenePage;
-import appli.schumanconnect.utils.StudentSingleton;
+import appli.schumanconnect.utils.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
@@ -19,20 +23,58 @@ import java.util.ResourceBundle;
 
 public class AssemblyDossierController implements Initializable {
 
-    @FXML
-    private Label StudentIdLabel;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Student StudentId = StudentSingleton.getInstance().getStudentId();
 
-        StudentIdLabel.setText("Etudiant ID: " + StudentId.getIdEtudiant());
 
-        loadDossierDetails(StudentId.getIdEtudiant());
     }
 
-    private void loadDossierDetails(int dossierId) {
+    @FXML
+    public void addStudent(){}
 
+
+
+
+
+    @FXML
+    public void logout(ActionEvent event) throws IOException {
+        UserConnectedSingleton.getInstance().logout();
+        ScenePage.switchView("/appli/schumanconnect/login-view.fxml", event);
+    }
+
+    @FXML
+    public void changePageSceneHome(ActionEvent event) throws IOException {
+        ScenePage.switchView("/appli/schumanconnect/homeView/homePage-view.fxml", event);
+    }
+
+    @FXML
+    public void changePageSceneDossierInscription(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/appli/schumanconnect/secretaryView/allStudents.fxml"));
+        Parent root = loader.load();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        Stage stage = (Stage) menuItem.getParentPopup().getOwnerWindow();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void changePageSceneAddStudent(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/appli/schumanconnect/secretaryView/addStudents.fxml"));
+        Parent root = loader.load();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        Stage stage = (Stage) menuItem.getParentPopup().getOwnerWindow();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void changePageSceneAddStudentButton(ActionEvent event) throws IOException{
+        ScenePage.switchView("/appli/schumanconnect/secretaryView/addStudents.fxml", event);
     }
 
 }
