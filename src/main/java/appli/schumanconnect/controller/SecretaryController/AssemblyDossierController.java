465087d1d8e65935e2e23107283b5968166289e6
N-ASSAF <a.nufil@lprs.fr>
2025-02-10 00:11:13 +0100

    update navbar with admin in AssemblyDossier page

--- src/main/java/appli/schumanconnect/controller/SecretaryController/AssemblyDossierController.java
+++ src/main/java/appli/schumanconnect/controller/SecretaryController/AssemblyDossierController.java
@@ -208,5 +208,18 @@ public class AssemblyDossierController implements Initializable {
         ScenePage.switchView("/appli/schumanconnect/secretaryView/addStudents.fxml", event);
     }
 
+
+    @FXML
+    public void changePageSceneAdmin(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/appli/schumanconnect/adminView/home-view.fxml"));
+        Parent root = loader.load();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        Stage stage = (Stage) menuItem.getParentPopup().getOwnerWindow();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
 
