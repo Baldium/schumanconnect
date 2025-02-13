56d8b3a33410387b2b6dc0bdfab4aaa3582d1191
N-ASSAF <a.nufil@lprs.fr>
2025-02-13 22:43:06 +0100

    Add list of users

--- src/main/java/appli/schumanconnect/controller/AdminController/PanelAdminController.java
+++ src/main/java/appli/schumanconnect/controller/AdminController/PanelAdminController.java
@@ -1,18 +1,69 @@
 package appli.schumanconnect.controller.AdminController;
 
+import appli.schumanconnect.model.Role;
+import appli.schumanconnect.model.User;
+import appli.schumanconnect.repository.UserRepository;
 import appli.schumanconnect.utils.ScenePage;
 import appli.schumanconnect.utils.UserConnectedSingleton;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
-public class PanelAdminController {
+
+public class PanelAdminController implements Initializable {
+    @FXML
+    private TableView<User> user_tableView;
+    @FXML
+    private TableColumn<User,Integer> user_id_column;
+    @FXML
+    private TableColumn<User,String> user_nom_column;
+    @FXML
+    private TableColumn<User,String> user_prenom_column;
+    @FXML
+    private TableColumn<User,String> user_mail_column;
+    @FXML
+    private TableColumn<User,String> user_role_column;
+    @FXML
+    private TableColumn<User,String> user_dernierco_column;
+
+
+    private ObservableList<User> user_list;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            this.user_list = UserRepository.getAllUser();
+            this.user_id_column.setCellValueFactory(new PropertyValueFactory<>("id_user"));
+            this.user_nom_column.setCellValueFactory(new PropertyValueFactory<>("nom"));
+            this.user_prenom_column.setCellValueFactory(new PropertyValueFactory<>("prenom"));
+            this.user_mail_column.setCellValueFactory(new PropertyValueFactory<>("email"));
+            this.user_role_column.setCellValueFactory(new PropertyValueFactory<>("role"));
+            this.user_dernierco_column.setCellValueFactory(new PropertyValueFactory<>("last_log"));
+
+            this.user_tableView.setItems(this.user_list);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     @FXML
     public void changePageSceneHome(ActionEvent event) throws IOException {
         ScenePage.switchView("/appli/schumanconnect/homeView/homePage-view.fxml", event);
@@ -76,4 +127,19 @@ public class PanelAdminController {
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    public void changePageSceneAdminAdd(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/appli/schumanconnect/adminView/addAdmin.fxml"));
+        Parent root = loader.load();
+        Button button = (Button) event.getSource();
+        Stage stage = (Stage) button.getScene().getWindow();
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
 }
---
+++ src/main/java/appli/schumanconnect/repository/UserRepository.java
@@ -0,0 +1,51 @@
+package appli.schumanconnect.repository;
+
+import appli.schumanconnect.controller.FlashMessage;
+import appli.schumanconnect.model.Role;
+import appli.schumanconnect.model.User;
+import appli.schumanconnect.utils.Bdd;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class UserRepository {
+    public static ObservableList<User> getAllUser() throws SQLException {
+        Connection my_bdd = Bdd.my_bdd();
+
+        try {
+            ObservableList<User> users = FXCollections.observableArrayList();
+            Role role;
+            PreparedStatement requete = my_bdd.prepareStatement("SELECT * FROM `users`");
+            ResultSet recup = requete.executeQuery();
+
+
+            while (recup.next()) {
+
+                String roleString = recup.getString("role");
+                if (Objects.equals(roleString, "PROFESSEUR")) {
+                    role = Role.PROFESSEUR;
+                } else if (Objects.equals(roleString, "SECRETAIRE")) {
+                    role = Role.SECRETAIRE;
+                } else if (Objects.equals(roleString, "GESTIONNAIRE")) {
+                    role = Role.SECRETAIRE;
+                } else {
+                    role = Role.ADMIN;
+                }
+
+                users.add(new User(recup.getInt("id_user"), recup.getString("nom"), recup.getString("prenom"), recup.getString("email"), recup.getString("mdp"), role, recup.getString("derniere_connexion")));
+            }
+            return users;
+
+        } catch (Exception e) {
+            FlashMessage.show("Erreur : " + e.getMessage(), Alert.AlertType.ERROR);
+
+            throw new RuntimeException(e);
+        }
+    }
+
+}
